3fbb7542f5726906d37a5e2a31b24543534efd9e
Leandro Souza <leandro.dev@icloud.com>
2022-04-30 14:46:39 -0300

    findbyId in UserResource

---
+++ src/main/java/com/leandro/apiexample/api/UserResource.java
@@ -0,0 +1,18 @@
+package com.leandro.apiexample.api;
+
+import com.leandro.apiexample.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+public class UserResource {
+
+    @GetMapping("/{id}")
+    public ResponseEntity<User> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok().body(User.builder().name("Leandro").build());
+    }
+}
---
+++ src/main/java/com/leandro/apiexample/domain/User.java
@@ -0,0 +1,25 @@
+package com.leandro.apiexample.domain;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+}
